1592386fcc47b7593bea9b0dec94024e1f456f11
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-02-11 18:19:39 +0800

    Changed PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[]posts;
+
+    public Post[] search(int id,int ownerId,String text){  // поиск поста по Id, автору поста, по содержанию поста
+
+        return posts;
+    }
+    public void delete(int id){  // удаление поста
+
+    }
+
+}
