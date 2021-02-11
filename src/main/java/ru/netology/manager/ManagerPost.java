f064165fc01d026c11ac1ed547e8ffd597f0c076
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-02-11 17:09:05 +0800

    Changed ManagerPost

--- src/main/java/ru/netology/manager/ManagerPost.java
+++ src/main/java/ru/netology/manager/ManagerPost.java
@@ -5,10 +5,6 @@ import ru.netology.domain.Post;
 public class ManagerPost {
     private Post[]posts;
 
-    public Post[] viewPosts(int id,int fromId,int ownerId,String text,String imageURL,String iconURL,String comment,boolean canButtonToReport){ //просмотр поста
-
-        return posts;
-    }
     public Post[] searchPosts(int id,int ownerId,String text){  // поиск поста по Id, автору поста, по содержанию поста
 
         return posts;
